6cce3dc3ceafd67e011ea018bdbecb0df1c59597
Jacob Samro <admin@jsam.in>
2016-02-08 14:57:15 +0530

    Update symboltable.cpp

--- Symbol-Table/symboltable.cpp
+++ Symbol-Table/symboltable.cpp
@@ -1,20 +1,16 @@
+/* 
+Compiled with GCC Compiler
+*/
+
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 using namespace std;
 
 int main(){
-    
-struct sy_tab
-{
-    char a[20];
-    char b[20];
-
-};
-
-char arr[5][40]={"int","char","double","float","long"};
 
 char key[32][10]={"break","auto","case","const","char","continue","default","do","double","else","enum","extern","float","for","goto","if","int","long","register","return","short","signed","sizeof","static","struct","switch","typedef","union","unsigned","void","volatile","while"};
 
@@ -32,21 +28,17 @@ char temp[10000];
 
 int memory = 10245;
 
+cout<<"Variable      DataType  Memory\n";
+
 while(fin)
 {
     fin.get(c);
     
-    
     temp[break_index] = c;
-    //cout<<strlen(temp)<<"\t";
 
     if(c==' ' || c == '\n' || c == ',' || c == '}') {
 
-        
-
-
-
-        //Clear the space
+  //Clear the space
         char newtemp[10000];
         int newIndex = 0;
         
@@ -54,11 +46,10 @@ while(fin)
             if(temp[_i] != ' '){
                 newtemp[newIndex] = temp[_i];
                 newIndex++;
-                
-            }
+                }
         }
 
-        //cout<<newtemp;
+        
         for(int __i = 0 ; __i <  strlen(newtemp) ; __i++){
             if(newtemp[__i] == ','  ||newtemp[__i] == ' ' || newtemp[__i] == ';' || newtemp[__i] == '('){
                 newtemp[__i] = '\0'; 
@@ -68,57 +59,57 @@ while(fin)
 
         char lastDataType[10];
 
-        
+        for(int _i = 0 ; _i < 32; _i++){
+            if(strcmp(key[_i], newtemp) == 0){  
+                
+                strcpy(lastDataType,key[_i]);
+                break;
+            }
+        }
 
-        if(strcmp(newtemp,"int")==0){
-            //cout<<"Keyword [ int ] : " << newtemp << "\n";
-            strcpy(lastDataType,"int");
-            memory += 2 ;
-        }else if(strcmp(newtemp,"char")==0){
-            //cout<<"Keyword [ char ] : " << newtemp << "\n";
-            strcpy(lastDataType,"char");
-            memory += 1 ;
-        }else if(strcmp(newtemp,"float")==0){
-            //cout<<"Keyword [ float ] : " << newtemp << "\n";   
-            strcpy(lastDataType,"float");
-            memory += 4 ;
-        }else if(newtemp[0] == '#'){
+        if(strcmp(newtemp,"int") == 0 || strcmp(newtemp,"char") == 0 || strcmp(newtemp,"float") == 0|| strcmp(newtemp,"double") == 0|| strcmp(newtemp,"long") == 0){
 
+        }
+        else if(newtemp[0] == '#'){
         }else if(strcmp(newtemp,"main")==0 || strcmp(newtemp,"return")==0){
-
         }
-        else if(newtemp[0] == ' '){
-        }
+        else if(newtemp[0] == ' '){        }
         else if(newtemp[0] == '<' || newtemp[0] == '}' || newtemp[0] == '{'){
         }
         else if(newtemp[0] == '\n'){
-        }
-        else{
-            cout<<"Variable  : " << newtemp << " : DataType : " << lastDataType << " : Memory : " << memory << "\n";
-        }
-        
+        }else if(strcmp(lastDataType,"int")==0){
+            memory += 2 ;
+            cout<< "    "<<newtemp << "   \t " << lastDataType << "\t " << memory << "\n";
+        }else if(strcmp(lastDataType,"char")==0){
+            memory += 1 ;
+            cout<< "    "<<newtemp << "   \t " << lastDataType << "\t " << memory << "\n";
 
-       
-        
+        }else if(strcmp(lastDataType,"float")==0){
+            memory += 4 ;
+            cout<< "    "<<newtemp << "   \t " << lastDataType << "\t " << memory << "\n";
+            
+        }else if(strcmp(lastDataType,"double")==0){
+            memory += 4 ;
+            cout<< "    "<<newtemp << "   \t " << lastDataType << "\t " << memory << "\n";
+            
+        }else if(strcmp(lastDataType,"long")==0){
+            memory += 6 ;
+            cout<< "    "<<newtemp << "   \t " << lastDataType << "\t " << memory << "\n";
+        }
+
         break_index = 0 ;
-
+        
         //Clear the variable temp and new temp
         for ( int i = 0 ; i< 40; i++){
             newtemp[i] = ' ';
             temp[i] = ' ';
         }
-            
 
     }else{
-        
         
     }
 
     break_index++; 
 
 }
-
-  
-
 }
- 
